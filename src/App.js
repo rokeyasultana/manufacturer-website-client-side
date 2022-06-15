deac28d6a3f41bf59b8741e7a9cabc1ac697f9ce
jakiasultana2022 <rokeyasbd@gmail.com>
2022-06-15 09:52:55 +0600

    require-auth

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import SignUp from './Pages/Login/SignUp';
 import Footer from './Pages/Shared/Footer';
+
 function App() {
   return (
     <div className="App">
+ 
 <Routes>
 <Route path='/' element={<Home></Home>}></Route>
 <Route path='/login' element={<Login></Login>}></Route>
